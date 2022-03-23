52cae7e938d93f54ccdef4bdf1f7137e8d129708
KreslavskiKD <kirill.kreslavski@gmail.com>
2022-03-23 17:17:53 +0300

    a new scratch with korGE

--- README.md
+++
@@ -1,2 +0,0 @@
-# hse-2022-java-project
-Java project of HSE students in 2022
---
+++ android/src/main/java/MainActivity.kt
@@ -0,0 +1,8 @@
+package ru.hse.fmcs.tickgame
+import com.soywiz.korgw.KorgwActivity
+import main
+class MainActivity : KorgwActivity() {
+	override suspend fun activityMain() {
+		main()
+	}
+}
