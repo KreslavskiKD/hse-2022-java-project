13a116cae73c659b9a545ad18039d58799c5d24c
KreslavskiKD <kirill.kreslavski@gmail.com>
2022-06-26 14:23:00 +0300

    ui beautification and unification, alert dialog is now pretty

--- android/src/main/java/ru/hse/fmcs/tickgame/fragments/StartMenuFragment.kt
+++ android/src/main/java/ru/hse/fmcs/tickgame/fragments/StartMenuFragment.kt
@@ -8,6 +8,7 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -46,10 +47,13 @@ class StartMenuFragment : Fragment() {
         }
 
         connectBtn.setOnClickListener {
-            val alert: AlertDialog.Builder = AlertDialog.Builder(context)
+            val alert: AlertDialog.Builder = AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_DARK)
             var address : String
             val edittext = EditText(context)
             edittext.setText(GameContext.getServerAddress())
+            edittext.setTextColor(resources.getColor(R.color.white))
+            edittext.imeOptions = EditorInfo.IME_FLAG_NO_EXTRACT_UI
+            edittext.setSingleLine()
             alert.setMessage("")
             alert.setTitle("Enter server address")
 
